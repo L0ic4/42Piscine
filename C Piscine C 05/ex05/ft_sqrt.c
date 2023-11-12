352ac52cec3b3e4c7f2449aff055096128b95793
Loica Ny Tina Andriantsoa <lny-tina@c1r9s11.42antananarivo.mg>
2023-11-12 17:14:02 +0300

    fix C05 ex05

--- C Piscine C 05/ex05/ft_sqrt.c
+++ C Piscine C 05/ex05/ft_sqrt.c
@@ -12,16 +12,16 @@
 
 int	ft_sqrt(int nb)
 {
-	int	i;
+	unsigned int		sqrt;
+	unsigned int		index;
 
-	if (nb == 0)
+	if (nb < 0)
 		return (0);
-	i = 1;
-	while (i <= nb / 2)
-	{
-		if (i * i == nb)
-			return (i);
-		i++;
-	}
-	return (0);
+	if (nb <= 1)
+		return (nb);
+	index = 0;
+	while ((sqrt = index * index) <= (unsigned int)nb)
+		index++;
+	index -= 1;
+	return (index * index == (unsigned int)nb ? index : 0);
 }
