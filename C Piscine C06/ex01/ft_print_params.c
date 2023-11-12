e6a85869f1dd17c60a872e1fd5528be88cd16460
Loica Ny Tina Andriantsoa <lny-tina@c1r9s11.42antananarivo.mg>
2023-11-12 14:40:54 +0300

    verify C06

--- C Piscine C06/ex01/ft_print_params.c
+++ C Piscine C06/ex01/ft_print_params.c
@@ -6,11 +6,24 @@
 /*   By: lny-tina <lny-tina@student.42antananarivo. +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 13:42:45 by lny-tina          #+#    #+#             */
-/*   Updated: 2023/11/11 13:42:45 by lny-tina         ###   ########.fr       */
+/*   Updated: 2023/11/12 14:36:41 by lny-tina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+
+int	ft_strlen(char *arg)
+{
+	int	count;
+
+	count = 0;
+	while (*arg != '\0')
+	{
+		count++;
+		arg++;
+	}
+	return (count);
+}
 
 int	main(int argc, char *argv[])
 {
@@ -21,7 +34,7 @@ int	main(int argc, char *argv[])
 	i = 1;
 	while (i < argc)
 	{
-		write(1, argv[i], strlen(argv[i]));
+		write(1, argv[i], ft_strlen(argv[i]));
 		write(1, "\n", 1);
 		i++;
 	}
--- C Piscine C06/ex02/ft_rev_params.c
+++ C Piscine C06/ex02/ft_rev_params.c
@@ -12,6 +12,19 @@
 
 #include <unistd.h>
 
+int	ft_strlen(char *arg)
+{
+	int	count;
+
+	count = 0;
+	while (*arg != '\0')
+	{
+		count++;
+		arg++;
+	}
+	return (count);
+}
+
 int	main(int argc, char *argv[])
 {
 	int	i;
@@ -21,7 +34,7 @@ int	main(int argc, char *argv[])
 	i = argc -1;
 	while (i > 0)
 	{
-		write(1, argv[i], strlen(argv[i]));
+		write(1, argv[i], ft_strlen(argv[i]));
 		write(1, "\n", 1);
 		i--;
 	}
